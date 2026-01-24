6be987c34a82171acf96063258fac70ce086abc5
karhunenloeve <accounts.pointed217@passinbox.com>
2026-01-24 17:34:21 +0100

    Publish docs to GitHub Pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,15 +2,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath("../../src"))
 
+project = "Homolipop"
+author = "Luciano Melodia"
+release = "0.1.0"
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
-
 html_theme = "furo"
-
-project = "Homolipop"
-author = "Luciano Melodia"
-copyright = "2026, Luciano Melodia"
-release = "1.0.0"
